c64ea88ff833c2216c9fa64097e75a9f88ae9bd4
pricea21 <pricea26@my.erau.edu>
2023-10-09 21:34:33 -0700

    help

---
+++ Assets/Scripts/PuzzleMaze.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PuzzleMaze : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("PuzzleMaze");
+    }
+
+    public void PlatformJumper()
+    {
+        SceneManager.LoadScene("PlatformJumper");
+    }
+
+    public void PaparazziFlash()
+    {
+        SceneManager.LoadScene("PaparazziFlash");
+    }
+
+    public void Jeopardy()
+    {
+        SceneManager.LoadScene("Jeopardy");
+    }
+}
